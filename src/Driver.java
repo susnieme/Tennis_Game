d4e8c8c3b63a398b9fcbae4d56bf08b8a33d4124
Iflaah02 <iflaahjet@hotmail.com>
2018-09-12 13:33:03 +0300

    adding project

---
+++ src/Driver.java
@@ -0,0 +1,10 @@
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TennisGame play = new TennisGame();
+		play.getScore();
+	}
+
+}
---
+++ src/TennisGame.java
@@ -0,0 +1,34 @@
+
+public class TennisGame {
+	
+	private int p1;
+	private int p2;
+	private int scoreFlag;
+	
+	public TennisGame() {
+		// TO BE IMPLEMENTED
+		System.out.println("I am constructor");
+		
+	}
+
+	public String getScore() {
+		System.out.println("I am method for displaying score");
+		// Here is the format of the scores: "player1Score - player2Score"
+		// "0 - 0"
+		// "15 - 15"
+		// "30 - 30"
+		// "deuce"
+		// "15 - 0", "0 - 15"
+		// "30 - 0", "0 - 30"
+		// "40 - 0", "0 - 40"
+		// "30 - 15", "15 - 30"
+		// "40 - 15", "15 - 40"
+		// "advantage player1"
+		// "advantage player2"
+		// "game player1"
+		// "game player2"
+
+		// TO BE IMPLEMENTED
+		return "";
+	}
+}
