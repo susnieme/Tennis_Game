903741d323aac4e737df54742e24b262d87f85b4
susnieme <sutuni@hotmail.com>
2018-09-12 17:36:27 +0300

    Assignment Finished
    Logic done to the Tennis Game.

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -1,9 +1,9 @@
 
 public class TennisGame {
 	
-	private int p1;
-	private int p2;
-	private int scoreFlag;
+	private int p1 = 0;
+	private int p2 = 0;
+	private int scoreFlag = 0;
 	
 	public TennisGame() {
 		// TO BE IMPLEMENTED
@@ -28,7 +28,72 @@ public class TennisGame {
 		// "game player1"
 		// "game player2"
 
-		// TO BE IMPLEMENTED
-		return "";
+		if (hasWinner()) {
+			return playerWithHighestScore() + " wins";
+		}
+		
+		if (hasAdvantage()) { 
+			return "Advantage " + playerWithHighestScore(); 
+		}
+		
+		if (isDeuce())
+			return "Deuce";
+		
+		if(p1 == p2) {
+			return translateScore(p1) + " all";
+		}
+		
+		return translateScore(p1) + " - " + translateScore(p2);
+	}
+	private boolean isDeuce() {
+		return p1 >= 3 && p2 == p1;
+	}
+	
+	private String playerWithHighestScore() {
+		if (p1 > p2) {
+			return "player1";
+		} else {
+			return "player2";
+		}
+	}
+	
+	private boolean hasWinner() {
+		if(p2 >= 4 && p2 >= p1 + 2 )
+			return true;
+		if(p1 >= 4 && p1 >= p2 + 2)
+			return true;
+		return false;
+	}
+	
+	private boolean hasAdvantage() {
+		if (p2 >= 4 && p2 == p1 + 1)
+			return true;
+		if (p1 >= 4 && p1 == p2 + 1)
+			return true;
+		
+		return false;
+
+	}
+
+	public void p1s() {
+		p1++;
+	}
+
+	public void p2s() {
+		p2++;
+	}
+	
+	private String translateScore(int score) {
+		switch (score) {
+		case 3:
+			return "Forty";
+		case 2:
+			return "Thirty";
+		case 1: 
+			return "Fifteen";
+		case 0:
+			return "Love";
+		}
+		throw new IllegalArgumentException("Illegal score: " + score);
 	}
 }
