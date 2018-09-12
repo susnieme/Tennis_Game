61a2623410c3dfbf5e7c06decbebee2035de69d3
susnieme <sutuni@hotmail.com>
2018-09-12 18:03:55 +0300

    Test input

--- src/Driver.java
+++ src/Driver.java
@@ -1,9 +1,29 @@
-
+import java.util.Scanner;
 public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	    Scanner scan= new Scanner(System.in);
+
+	    //For string
+
+	    String text= scan.nextLine();
+
+	    System.out.println(text);
+
+	    //for int
+
+	    int num= scan.nextInt();
+
+	    System.out.println(num);
+	    
 		TennisGame play = new TennisGame();
+		if (num == 1) {
+			play.p1s();
+		}
+		if (num == 2) {
+			play.p2s();
+		}		
 		play.getScore();
 	}
 
